6e514eb5c0690702830d8f1cbfddd43b87bfa0b1
Alejandra Cárdenas <alejandra.cardenas@pyxisportal.co>
2022-07-08 09:11:24 -0500

    Fix vagranfile

--- Vagrantfile
+++ Vagrantfile
@@ -6,52 +6,13 @@ Vagrant.configure("2") do |config|
   svName = "ubuntu-server"
   config.vm.synced_folder "./www/", "/var/www/html"
 
-  config.vm.define svName do |vm1|
-    vm1.vm.hostname = svName
-    vm1.vm.box = "bento/ubuntu-22.04"
-    vm1.vm.network "private_network", ip: "192.168.56.10"
-
-    vm1.vm.provider "virtualbox" do |vb|
-      vb.name = svName
-      vb.gui = false
-      vb.memory = "1024"
-    end
-
-    vm1.vm.provision "shell", inline: <<-SHELL
-      #sudo apt-get update -y
-	    #sudo apt-get upgrade -y
-	    #sudo apt-get install net-tools
-	    #sudo apt-get install apache2 -y
-	    #sudo apt-get install php libapache2-mod-php php-mysql -y
-	
-	    apt-get install apache2 -y
-      apt-get install libapache2-mod-php -y
-      apt-get install php-mysql -y
-      apt-get install mysql-client -y
-
-      # Enable apache mod_rewrite
-      a2enmod rewrite
-      a2enmod actions
-
-      # Change AllowOverride from None to All
-      sed -i '170,174 s/AllowOverride None/AllowOverride All/g' /etc/apache2/apache2.conf
-
-      # Start the webserver
-      service apache2 restart
-    SHELL
-
-    vm1.vm.provision "shell", run: "always", inline: <<-SHELL
-        echo "Hello from the Ubuntu VM"
-    SHELL
-  end
-
   config.vm.define dbName do |db|	
     db.vm.hostname = dbName
     db.vm.box = "bento/centos-stream-8"
     db.vm.box_check_update = true 
       
     db.vm.network "forwarded_port", guest: 3306, host: 3306, host_ip: "127.0.0.1"
-    db.vm.network "private_network", ip: "192.168.33.20"
+    db.vm.network "private_network", ip: "192.168.56.20"
 
     db.vm.synced_folder "./SyncFolder", "/vagrant_data/SyncFolder"
 
@@ -72,6 +33,30 @@ Vagrant.configure("2") do |config|
 
       sudo mysqladmin -u root password vagrant
       cat /vagrant_data/SyncFolder/init.sql | sudo mysql -u root -pvagrant
+    SHELL
+  end
+
+  config.vm.define svName do |vm2|
+    vm2.vm.hostname = svName
+    vm2.vm.box = "bento/ubuntu-22.04"
+    vm2.vm.network "private_network", ip: "192.168.56.10"
+
+    vm2.vm.provider "virtualbox" do |vb|
+      vb.name = svName
+      vb.gui = false
+      vb.memory = "1024"
+    end
+
+    vm2.vm.provision "shell", inline: <<-SHELL
+      sudo apt-get update -y
+      sudo apt-get upgrade -y
+      sudo apt-get install net-tools
+      sudo apt-get install apache2 php7.2 libapache2-mod-php php-mysql -y
+      sudo service apache2 restart
+    SHELL
+
+    vm2.vm.provision "shell", run: "always", inline: <<-SHELL
+      echo "Hello from the Ubuntu VM"
     SHELL
   end
   
--- www/index.php
+++ www/index.php
@@ -1,128 +1,86 @@
-<?php
-include("conexion.php");
-?>
 <!DOCTYPE html>
-<html lang="es">
+<html lang="en">
 <head>
-
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Datos de empleados</title>
-
-	<!-- Bootstrap -->
-	<link href="css/bootstrap.min.css" rel="stylesheet">
-	<link href="css/style_nav.css" rel="stylesheet">
-
-	<style>
-		.content {
-			margin-top: 80px;
-		}
-	</style>
-
+    <meta charset="UTF-8">
+    <title>Dashboard</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <style>
+        .wrapper{
+            width: 600px;
+            margin: 0 auto;
+        }
+        table tr td:last-child{
+            width: 120px;
+        }
+    </style>
+    <script>
+        $(document).ready(function(){
+            $('[data-toggle="tooltip"]').tooltip();   
+        });
+    </script>
 </head>
 <body>
-	<nav class="navbar navbar-default navbar-fixed-top">
-		<?php include('nav.php');?>
-	</nav>
-	<div class="container">
-		<div class="content">
-			<h2>Lista de empleados</h2>
-			<hr />
-
-			<?php
-			if(isset($_GET['aksi']) == 'delete'){
-				// escaping, additionally removing everything that could be (html/javascript-) code
-				$nik = mysqli_real_escape_string($con,(strip_tags($_GET["nik"],ENT_QUOTES)));
-				$cek = mysqli_query($con, "SELECT * FROM empleados WHERE codigo='$nik'");
-				if(mysqli_num_rows($cek) == 0){
-					echo '<div class="alert alert-info alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button> No se encontraron datos.</div>';
-				}else{
-					$delete = mysqli_query($con, "DELETE FROM empleados WHERE codigo='$nik'");
-					if($delete){
-						echo '<div class="alert alert-success alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button> Datos eliminado correctamente.</div>';
-					}else{
-						echo '<div class="alert alert-danger alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button> Error, no se pudo eliminar los datos.</div>';
-					}
-				}
-			}
-			?>
-
-			<form class="form-inline" method="get">
-				<div class="form-group">
-					<select name="filter" class="form-control" onchange="form.submit()">
-						<option value="0">Filtros de datos de empleados</option>
-						<?php $filter = (isset($_GET['filter']) ? strtolower($_GET['filter']) : NULL);  ?>
-						<option value="1" <?php if($filter == 'Tetap'){ echo 'selected'; } ?>>Fijo</option>
-						<option value="2" <?php if($filter == 'Kontrak'){ echo 'selected'; } ?>>Contratado</option>
-                        <option value="3" <?php if($filter == 'Outsourcing'){ echo 'selected'; } ?>>Outsourcing</option>
-					</select>
-				</div>
-			</form>
-			<br />
-			<div class="table-responsive">
-			<table class="table table-striped table-hover">
-				<tr>
-                    <th>No</th>
-					<th>Código</th>
-					<th>Nombre</th>
-                    <th>Lugar de nacimiento</th>
-                    <th>Fecha de nacimiento</th>
-					<th>Teléfono</th>
-					<th>Cargo</th>
-					<th>Estado</th>
-                    <th>Acciones</th>
-				</tr>
-				<?php
-				if($filter){
-					$sql = mysqli_query($con, "SELECT * FROM empleados WHERE estado='$filter' ORDER BY codigo ASC");
-				}else{
-					$sql = mysqli_query($con, "SELECT * FROM empleados ORDER BY codigo ASC");
-				}
-				if(mysqli_num_rows($sql) == 0){
-					echo '<tr><td colspan="8">No hay datos.</td></tr>';
-				}else{
-					$no = 1;
-					while($row = mysqli_fetch_assoc($sql)){
-						echo '
-						<tr>
-							<td>'.$no.'</td>
-							<td>'.$row['codigo'].'</td>
-							<td><a href="profile.php?nik='.$row['codigo'].'"><span class="glyphicon glyphicon-user" aria-hidden="true"></span> '.$row['nombres'].'</a></td>
-                            <td>'.$row['lugar_nacimiento'].'</td>
-                            <td>'.$row['fecha_nacimiento'].'</td>
-							<td>'.$row['telefono'].'</td>
-                            <td>'.$row['puesto'].'</td>
-							<td>';
-							if($row['estado'] == '1'){
-								echo '<span class="label label-success">Fijo</span>';
-							}
-                            else if ($row['estado'] == '2' ){
-								echo '<span class="label label-info">Contratado</span>';
-							}
-                            else if ($row['estado'] == '3' ){
-								echo '<span class="label label-warning">Outsourcing</span>';
-							}
-						echo '
-							</td>
-							<td>
-
-								<a href="edit.php?nik='.$row['codigo'].'" title="Editar datos" class="btn btn-primary btn-sm"><span class="glyphicon glyphicon-edit" aria-hidden="true"></span></a>
-								<a href="index.php?aksi=delete&nik='.$row['codigo'].'" title="Eliminar" onclick="return confirm(\'Esta seguro de borrar los datos '.$row['nombres'].'?\')" class="btn btn-danger btn-sm"><span class="glyphicon glyphicon-trash" aria-hidden="true"></span></a>
-							</td>
-						</tr>
-						';
-						$no++;
-					}
-				}
-				?>
-			</table>
-			</div>
-		</div>
-	</div><center>
-	<p>&copy; Sistemas Web <?php echo date("Y");?></p
-		</center>
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-	<script src="js/bootstrap.min.js"></script>
+    <div class="wrapper">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="mt-5 mb-3 clearfix">
+                        <h2 class="pull-left">Employees Details</h2>
+                        <a href="create.php" class="btn btn-success pull-right"><i class="fa fa-plus"></i> Add New Employee</a>
+                    </div>
+                    <?php
+                    // Include config file
+                    require_once "config.php";
+                    
+                    // Attempt select query execution
+                    $sql = "SELECT * FROM employees";
+                    if($result = mysqli_query($link, $sql)){
+                        if(mysqli_num_rows($result) > 0){
+                            echo '<table class="table table-bordered table-striped">';
+                                echo "<thead>";
+                                    echo "<tr>";
+                                        echo "<th>#</th>";
+                                        echo "<th>Name</th>";
+                                        echo "<th>Address</th>";
+                                        echo "<th>Salary</th>";
+                                        echo "<th>Action</th>";
+                                    echo "</tr>";
+                                echo "</thead>";
+                                echo "<tbody>";
+                                while($row = mysqli_fetch_array($result)){
+                                    echo "<tr>";
+                                        echo "<td>" . $row['id'] . "</td>";
+                                        echo "<td>" . $row['name'] . "</td>";
+                                        echo "<td>" . $row['address'] . "</td>";
+                                        echo "<td>" . $row['salary'] . "</td>";
+                                        echo "<td>";
+                                            echo '<a href="read.php?id='. $row['id'] .'" class="mr-3" title="View Record" data-toggle="tooltip"><span class="fa fa-eye"></span></a>';
+                                            echo '<a href="update.php?id='. $row['id'] .'" class="mr-3" title="Update Record" data-toggle="tooltip"><span class="fa fa-pencil"></span></a>';
+                                            echo '<a href="delete.php?id='. $row['id'] .'" title="Delete Record" data-toggle="tooltip"><span class="fa fa-trash"></span></a>';
+                                        echo "</td>";
+                                    echo "</tr>";
+                                }
+                                echo "</tbody>";                            
+                            echo "</table>";
+                            // Free result set
+                            mysqli_free_result($result);
+                        } else{
+                            echo '<div class="alert alert-danger"><em>No records were found.</em></div>';
+                        }
+                    } else{
+                        echo "Oops! Something went wrong. Please try again later.";
+                    }
+ 
+                    // Close connection
+                    mysqli_close($link);
+                    ?>
+                </div>
+            </div>        
+        </div>
+    </div>
 </body>
 </html>
